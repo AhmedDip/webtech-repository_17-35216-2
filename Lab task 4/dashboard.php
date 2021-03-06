cf3bc0772bafdd8cbb7010e906916c1a45ad4955
Ahmed Rasidun Bari Dip <67531074+AhmedDip@users.noreply.github.com>
2021-03-06 21:23:37 +0600

    forth commit

---
+++ Lab task 4/dashboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php include 'add.php';?>
+    <article>
+        <h3>welcome </h3>
+    </article>
+    <?php include 'include/footer.php';?>
+</body>
+</html>
